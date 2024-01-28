01fd1baea856dedefde5f21ab7fac48298c3563f
VICTOR CHEGE <98538799+Deuce01@users.noreply.github.com>
2024-01-28 16:52:32 +0300

    Create PositiveNegativeZeroChecker.java

---
+++ PositiveNegativeZeroChecker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NumberSignChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+
+        scanner.close();
+    }
+          }

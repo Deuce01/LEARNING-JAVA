4e460ad8cfea5233ca04646b1d5f75a61bc2bd58
VICTOR CHEGE <98538799+Deuce01@users.noreply.github.com>
2024-02-04 20:47:42 +0300

    Create ReadFile.java

---
+++ ReadFile.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            File file = new File("input.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+    }
+}

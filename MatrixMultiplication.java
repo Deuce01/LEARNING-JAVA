6514d82c31cae796f655f33cedc9317d0998cf32
VICTOR CHEGE <98538799+Deuce01@users.noreply.github.com>
2024-02-09 22:06:01 +0300

    Create MatrixMultiplication.java

---
+++ MatrixMultiplication.java
@@ -0,0 +1,24 @@
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] matrix1 = {{1, 2}, {3, 4}}; // Change the matrices as desired
+        int[][] matrix2 = {{5, 6}, {7, 8}};
+
+        int[][] result = new int[matrix1.length][matrix2[0].length];
+
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix2[0].length; j++) {
+                for (int k = 0; k < matrix2.length; k++) {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        // Print the result matrix
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
